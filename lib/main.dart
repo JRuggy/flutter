6e1077d6fa69ff7d30c653fa42ad3da98b16dd8c
JastonRuggy <jastonruggy@icloud.com>
2023-01-10 20:55:32 +0300

    eff

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, annotate_overrides
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Ruggy Trial'),
+    );
+  }
+}
